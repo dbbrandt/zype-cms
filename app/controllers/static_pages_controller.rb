a4674a0edd13609af70c571d813e980dd9bac708
Daniel Brandt <dbbrandt@gmail.com>
2017-06-19 12:43:13 -0400

    Work in progress on detail video.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,11 @@ class StaticPagesController < ApplicationController
   def show
     @video_id = params[:id]
     @item = DetailPage.instance rescue nil
+    if @video_id == "1"
+      @video_image = 'sarcastic-eddie-murphy.jpg'
+    else
+      @video_image = 'visions.jpg'
+    end
     @video = nil
   end
 
